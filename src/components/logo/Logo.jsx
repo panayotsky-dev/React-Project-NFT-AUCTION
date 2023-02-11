e2ac37fa9cebcf35978ecbb323dd470f274fbf0c
panayotsky.dev <panayotsky.dev@gmail.com>
2023-02-11 03:26:40 +0200

    Update Logo.jsx

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,18 +1,18 @@
 import React,{ useState} from 'react'
 
-function Logo() {
+function Logo({type= "default"}) {
   const [defaultImg, setClicked] = useState(false);
-  let muted = {muted:"/images/logo-muted.svg"}
-  let defaultLogo = { default:"/images/logo.svg"}
-  let type = "default"
+  let muted = "/images/logo-muted.svg"
+  let defaultLogo = "/images/logo.svg"
+ 
   const handleClick = () => {
     setClicked(!defaultImg)
   }
   return (
     <>
     <img
-    type={type}
-    // type={!defaultImg ? "default" : "muted"  }
+    
+    type={!defaultImg ? "default" : "muted"  }
     onClick={handleClick}
     src={!defaultImg ? defaultLogo : muted}
     alt={!defaultImg ? "default" : "muted" }    
@@ -22,3 +22,18 @@ function Logo() {
 }
 
 export default Logo
+
+
+
+// function Logo({type = "default"}) {
+//   let muted = "/images/logo-muted.svg"
+//     let defaultLogo = "/images/logo.svg"
+//     if (type === "default") {
+//       return React.createElement("img", { src: defaultLogoUrl }, null);
+//   }
+
+//   return React.createElement("img", { src: mutedLogoUrl }, null);
+
+// }
+
+// export default Logo
