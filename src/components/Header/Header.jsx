29ddf3aaaaccd62e134016e49870fc4c7e119b3a
panayotsky.dev <panayotsky.dev@gmail.com>
2023-02-14 02:56:47 +0200

    asd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react'
 import Grid from '@mui/material/Grid'
 import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
 import Button from '@mui/material/Button';
 import Logo from '../Logo/Logo';
-import { Toolbar } from '@mui/material';
+
+
 
 // import withStyles from '@material-ui/core/styles';
 
@@ -25,52 +25,25 @@ import { Toolbar } from '@mui/material';
 
 function Header() {
   return (
-//     <>   
-//    <Toolbar maxwidt="sm">
-//     <Grid container spacing={6}>
-//         <Grid item xl>
-//         <Logo />
-//         </Grid>
-//         <Grid item xl>
-//         <TextField
-//           id="outlined-helperText"
-//           label="Search bar"
-//           defaultValue=""
-//         //   helperText="Some important text"
-//         />
-
-//             {/* <input type='text' placeholder='Can we help you ?' onChange='{handleChange}' value='{searchInput}' /> */}
-//         </Grid>
-//         <Grid item xs>
-//         <Button variant="text">Text</Button>
-//         <Button variant="text">Text</Button>
-//         <Button variant="contained">Contained</Button>
-//         </Grid>
-//     </Grid>   
-//     </Toolbar> 
-    
-//     </>
-<Container className={classNames(styles.container)} maxWidth="xl"> 
-
-<AppBar position="static">
-
-  <Toolbar maxwidth="sm"> 
-  <Grid container spacing={1}>
-  <Grid item xs={2}>
-    <Item><Logo/></Item>
-  </Grid>
-  <Grid item xs={4}>
-    <Item><SearchBar/></Item>
-  </Grid>
-  <Grid item xs>
-    <Item><MenuButtons/></Item>
-  </Grid>
-</Grid>
- </Toolbar>
-
-</AppBar>
-
-</Container>
+  <>      
+    <Grid container spacing={6}>
+        <Grid item xl>
+        <Logo />
+        </Grid>
+        <Grid item xl>
+        <TextField
+          id="outlined-helperText"
+          label="Search bar"
+          defaultValue=""        
+        />           
+        </Grid>
+        <Grid item xs>
+        <Button variant="text">Text</Button>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        </Grid>
+    </Grid> 
+    </>
   )
 }
 
