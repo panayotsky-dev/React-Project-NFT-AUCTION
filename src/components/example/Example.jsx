92fd2c5413500e819f4b44ee97cd1be5f920c7a3
panayotsky.dev <panayotsky.dev@gmail.com>
2023-02-14 02:29:24 +0200

    =-=

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -4,16 +4,21 @@ import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
 import Logo from "../logo/Logo";
+import Header from "../Header/Header";
+
+
 
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
+      <Header />
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">
             Hi
           </Link>
+          
           <Logo type='default' />
         </Paper>
       </Container>
