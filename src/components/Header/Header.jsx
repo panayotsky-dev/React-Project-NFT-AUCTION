4df99d4a3bf77d980a6a6da751fb82ee1f2b11c6
panayotsky.dev <panayotsky.dev@gmail.com>
2023-02-14 02:40:37 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,31 +25,52 @@ import { Toolbar } from '@mui/material';
 
 function Header() {
   return (
-    <>   
-   <Toolbar maxwidt="sm">
-    <Grid container spacing={6}>
-        <Grid item xl>
-        <Logo />
-        </Grid>
-        <Grid item xl>
-        <TextField
-          id="outlined-helperText"
-          label="Search bar"
-          defaultValue=""
-        //   helperText="Some important text"
-        />
+//     <>   
+//    <Toolbar maxwidt="sm">
+//     <Grid container spacing={6}>
+//         <Grid item xl>
+//         <Logo />
+//         </Grid>
+//         <Grid item xl>
+//         <TextField
+//           id="outlined-helperText"
+//           label="Search bar"
+//           defaultValue=""
+//         //   helperText="Some important text"
+//         />
 
-            {/* <input type='text' placeholder='Can we help you ?' onChange='{handleChange}' value='{searchInput}' /> */}
-        </Grid>
-        <Grid item xs>
-        <Button variant="text">Text</Button>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        </Grid>
-    </Grid>   
-    </Toolbar> 
+//             {/* <input type='text' placeholder='Can we help you ?' onChange='{handleChange}' value='{searchInput}' /> */}
+//         </Grid>
+//         <Grid item xs>
+//         <Button variant="text">Text</Button>
+//         <Button variant="text">Text</Button>
+//         <Button variant="contained">Contained</Button>
+//         </Grid>
+//     </Grid>   
+//     </Toolbar> 
     
-    </>
+//     </>
+<Container className={classNames(styles.container)} maxWidth="xl"> 
+
+<AppBar position="static">
+
+  <Toolbar maxwidth="sm"> 
+  <Grid container spacing={1}>
+  <Grid item xs={2}>
+    <Item><Logo/></Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item><SearchBar/></Item>
+  </Grid>
+  <Grid item xs>
+    <Item><MenuButtons/></Item>
+  </Grid>
+</Grid>
+ </Toolbar>
+
+</AppBar>
+
+</Container>
   )
 }
 
